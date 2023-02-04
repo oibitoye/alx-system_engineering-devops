643629cc07098e4ab249e8ec136e5099de152880
Yussuph Ibitoye <ibitoyeolawale@gmail.com>
2023-02-03 20:33:34 -0800

    regex

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# This regular expression will match School
+
+puts ARGV[0].scan(/School/).join()
---
+++ 0x06-regular_expressions/2-repetition_token_1.rb
@@ -0,0 +1,3 @@
+#!/usr/bin/env ruby
+#Regular expression to match given case
+puts ARGV[0].scan(/hb?tn/).join()
